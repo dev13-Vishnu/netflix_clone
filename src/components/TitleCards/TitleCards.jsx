11699dfea302ff6b6d43a9fa2219b76da67439d3
Vishnu P S <vishnu13three@gamil.com>
2025-02-06 10:47:58 +0530

    first commit

---
+++ src/components/TitleCards/TitleCards.jsx
@@ -0,0 +1,12 @@
+import './TitleCards.css';
+import React from 'react'
+
+const TitleCards = () => {
+  return (
+    <div className='titlecards'>
+      
+    </div>
+  )
+}
+
+export default TitleCards
