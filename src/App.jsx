db2dd98fea450f48f07568e9f26891ac5b68df93
Vishnu P S <vishnu13three@gamil.com>
2025-02-07 09:13:32 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import React from 'react'
+import React from "react";
+import Home from "./pages/Home/Home";
 
 const App = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  return <div>
+    <Home/>
+  </div>;
+};
 
-export default App
+export default App;
