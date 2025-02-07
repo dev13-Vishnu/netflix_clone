9a5d00435ffd057e1659de455c4a702dc0cb1fdb
Vishnu P S <vishnu13three@gamil.com>
2025-02-07 12:35:58 +0530

    Tmdb data api added to list movies and shows inside title cards

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,18 +1,32 @@
 import './TitleCards.css';
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import cards_data from '../../assets/cards/Cards_data'
 const TitleCards = ({title, category}) => {
 
   const cardsRef = useRef();
+  const [apiData, setApiData] = useState([]);
 
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiZDUzZmFjNTUxNjU4N2VkZWExNjBkNWY1ODAwY2RmMyIsIm5iZiI6MTczNzExOTU3OC45NDQsInN1YiI6IjY3OGE1NzVhOTNmNzQyY2MyOWFkMTM2ZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.HJCEb2QOJ1eYuGpoD6-TCgY2OlT3SRIIF574h9Lw4Uc'
+    }
+  };
+  
   const handleWheel = (event) => {
     event.preventDefault();
     cardsRef.current.scrollLeft += event.deltaY;
   }
 
   
-
   useEffect(()=> {
+
+    fetch(`https://api.themoviedb.org/3/movie/${category?category:"now_playing"}?language=en-US&page=1`, options)
+    .then(res => res.json())
+    .then(res => setApiData(res.results))
+    .catch(err => console.error(err));
+
     cardsRef.current.addEventListener('wheel',handleWheel);
   },[])
 
@@ -20,10 +34,10 @@ const TitleCards = ({title, category}) => {
     <div className='title-cards'>
       <h2>{title?title:'Popular on Netflix'}</h2>
       <div className="card-list" ref = {cardsRef}>
-        {cards_data.map((card,index)=>{
+        {apiData.map((card,index)=>{
           return <div className="card" key={index}>
-            <img src={card.image} alt="" />
-            <p>{card.name}</p>
+            <img src={`https://image.tmdb.org/t/p/w500`+card.backdrop_path} alt="" />
+            <p>{card.original_title}</p>
           </div>
         })}
       </div>
